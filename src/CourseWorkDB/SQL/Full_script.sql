fec81e33e582a74a05f753c1ef392e7befd0c41a
DarienTLOD <https://github.com/DarienTLOD>
2016-05-23 22:41:31 +0000

    Were created the tables that associated with companies and places for practice.

--- src/CourseWorkDB/SQL/Full_script.sql
+++ src/CourseWorkDB/SQL/Full_script.sql
@@ -8,6 +8,7 @@ use CourseWorkDB;
 Create Table Faculty
 (
     [id] int Primary Key Identity(1,1) not null,
+	[phone_number] int not null,
 	[faculty_name] nvarchar(20) not null,
 ) 
 
@@ -15,6 +16,7 @@ Create Table Ñathedra
 (
     [id] int Primary Key Identity(1,1) not null,
 	[cathedra_name] nvarchar(20) not null,
+	[phone_number] int not null,
 	[faculty_id] int Foreign Key References Faculty(id) not null
 ) 
 
@@ -33,12 +35,14 @@ Create Table Groups
 
 Create Table Lecturers 
 (
-    [lecturer_id] int Primary Key Identity(1,1) not null,
+    [id] int Primary Key Identity(1,1) not null,
 	[lecturer_name] nvarchar(20) not null,
 	[lecturer_surname] nvarchar(20) not null,
 	[lecturer_second_name] nvarchar(20) not null,
+	[phone_number] int not null,
 	[position] nvarchar(10) not null,
 	[id_curator_of_group] int not null,
+	[password] nvarchar(max) not null,
 	[cathedra_id] int Foreign Key References Ñathedra(id) not null
 ) 
 
@@ -48,6 +52,38 @@ Create Table Students
 	[student_name] nvarchar(20) not null,
 	[student_surname] nvarchar(20) not null,
 	[student_second_name] nvarchar(20) not null,
+	[phone_number] int not null,
 	[is_praepostor] bit not null, 
 	[group_id] int Foreign Key References Groups(id) not null
 ) 
+
+--Table that is associated with companies providing places for practice
+
+Create Table Companies
+(
+	[id] int Primary Key Identity(1,1) not null,
+	[address] nvarchar(50) not null,
+	[company_name] nvarchar(20) not null,
+	[phone_number] int not null,
+	[head_of_the_practice] nvarchar(50) not null
+)
+
+--Table that are associated with places for practice
+
+Create Table Places_for_practice
+(
+	[id] int Primary Key Identity(1,1) not null,
+	[request] nvarchar(500),
+	[description] nvarchar(500),
+	[date_of_the_beginning] date not null,
+	[date_of_the_ending] date not null,
+	[company_id] int Foreign Key References Companies(id) not null
+)
+
+Create Table Students_at_practice
+(
+	[id] int Primary Key Identity(1,1) not null,
+	[places_for_practice_id] int Foreign Key References Places_for_practice(id) not null,
+	[student_id] int Foreign Key References Students(student_record_book_id) not null,
+	[curator_Of_practice_id] int Foreign Key References Lecturers(id) not null
+)
